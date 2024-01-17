6061672fba71a3243d5d20cb3463ba0338ae7c5e
Jess <jessica.wang360@gmail.com>
2024-01-16 22:31:06 -0800

    fix endpoint

--- src/FileUploadingForm.tsx
+++ src/FileUploadingForm.tsx
@@ -24,7 +24,7 @@ const FileUploadForm: React.FC = () => {
         formData.append('file', file);
 
         try {
-            const response = await axios.post('http://localhost:8080/upload', formData, {
+            const response = await axios.post('https://warp-theme-converter.vercel.app/upload', formData, {
                 headers: { 'Content-Type': 'multipart/form-data' },
             });
 
